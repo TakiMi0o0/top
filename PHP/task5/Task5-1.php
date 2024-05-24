d0bbeb25e265afccda62d87dcd438606d4e916b9
TakiMi0o0 <takimiworks@gmail.com>
2024-05-24 18:45:59 +0900

    Task5-1

--- PHP/task5/Task5-1.php
+++ PHP/task5/Task5-1.php
@@ -6,10 +6,15 @@
   <title>PHP練習</title>
 </head>
 <body>
+  <h1>九九の計算</h1>
   <?php
-
-
+    for($i = 1; $i <=9; $i++){
+      for($n = 1; $n <= 9; $n++){
+          $ans = $i * $n;
+          echo $i.' × '.$n.' = '.$ans.'　';
+      }
+      echo '<br>';
+    }
   ?>
-  <h1><?= $random_n ?>月は<?= $season ?>です。</h1>
 </body>
 </html>
