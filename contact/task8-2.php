414a44d425cd0f097a7e33b58664ce05ea6a17c1
TakiMi0o0 <takimiworks@gmail.com>
2024-07-01 19:24:32 +0900

    task8-2

---
+++ contact/task8-2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>完了ページ</title>
+</head>
+<body>
+  <h1 style="text-align:center;">送信完了しました。</h1>
+  <p style="text-align:center;"><a href="\contact\index.html">お問い合わせフォームに戻る</a></p>
+</body>
+</html> 
